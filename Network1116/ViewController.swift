0bc8bfd1e1bffbdb723db42728926a1f70efe9d7
a503-03 <shen_aihua0126@naver.com>
2018-11-16 13:02:58 +0900

    Initial Commit

---
+++ Network1116/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Network1116
+//
+//  Created by 503-03 on 2018. 11. 16..
+//  Copyright © 2018년 shenah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
